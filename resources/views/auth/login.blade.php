d25024ff385066ff3f7723a5e55b7d0150004c1e
Nikita Bulakhov <bulahov26@gmail.com>
2019-03-19 19:43:37 +0300

    add Login Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,34 @@
-<div class="loginTalkBgDiv"><img class="img-fluidv loginTalkBg" src="{{ asset('/images/Group10.png')}}"></div>
+{{-- <div class="loginTalkBgDiv"><img class="img-fluidv loginTalkBg" src="{{ asset('/images/Group10.png')}}"></div> --}}
 @extends('layouts.app')
+
+<style>
+    body{
+        background-image: url("/./images/Group10.png");
+        background-repeat: no-repeat;
+        background-position: center top;
+    }
+    .footerTalk{
+        top:40vh;
+    }
+</style>
 
 @section('content')
 <div class="container loginTalkContent">
-    <div class="row">
-        {{-- <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+    <div class="row justify-content-center">
+            <div class="panel panel-default col-md-8 col-lg-6 col-xl-5 loginTalkPanelLogin">
+                <div class="loginTalkBlurLayout"></div>
+                <div class="panel-heading h1" style="margin-bottom:1em">Log In</div>
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} ">
+                            <div class="loginTalkFormDiv">
+                                <label for="email" class="col-md-4 control-label">Email</label>
+                                <input id="email" type="email" class="col-md-6 mr-auto" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
+                                <br>
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
@@ -26,10 +37,11 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                            <div class="loginTalkFormDiv">
+                                <label for="password" class="col-md-4 control-label">Password</label>
+                                <input id="password" type="password" class="col-md-6 mr-auto" name="password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -38,34 +50,12 @@
                                 @endif
                             </div>
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
+                            <button type="submit" class="loginTalkLoginBtn">
+                                Login
+                            </button>
                     </form>
                 </div>
             </div>
-        </div> --}}
-        <div class="">
-
         </div>
     </div>
 </div>
